2287629453852e106881883581ca282318849e57
khoaled <56563374+khoaled@users.noreply.github.com>
2023-07-04 13:42:58 -0500

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -4,9 +4,12 @@
 hpath="/home/micah"
 rpath="/media/micah/micah/backups"
 
-echo "listing backups in the repo"
-borg list $rpath
+if [ /usr/bin/borg ]
+then
+  read -p "listing backups in the repo" -t 2
+  borg list $rpath
 
-echo "backing up $hpath"
-borg create --verbose --stats $rpath::backup-$(date +%F) $hpath
-
+  read -p "backing up $hpath" -t 2
+  borg create --verbose --stats $rpath::backup-$(date +%F) $hpath
+else
+  read -p "borg isn't installed." -t 2
