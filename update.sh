c5db73f03be362791bef4c5e6a082aa3642d061e
Micah Miller <miller.micah@gmail.com>
2023-07-21 15:58:33 -0500

    added sudo.

--- update.sh
+++ update.sh
@@ -3,13 +3,21 @@
 #
 if [ -x /usr/bin/apt ]
 then
-    apt update
-    apt -y full-upgrade
-    if [ -x /usr/bin/dnf]
+    sudo apt update
+    sudo apt -y full-upgrade
+    if [ -x /usr/bin/dnf ]
     then
-        dnf update
-        dnf -y upgrade
+        sudo dnf update
+        sudo dnf -y upgrade
+    else
+        echo "no dnf here"
     fi
 else
     echo "nothing to do, exiting"
+fi
+if [ -x /usr/bin/flatpak ]
+then
+    sudo flatpak -y update
+else
+    echo "no flatpak"
 fi
