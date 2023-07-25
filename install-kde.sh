9b86f308a99f42bc1ac0b46956635f4cba08bcfd
Micah Miller <miller.micah@gmail.com>
2023-07-25 16:36:00 -0500

    added sudo

---
+++ install-kde.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+# This is for a very minimal install of Gnome on Debian.
+#
+ksd="kde-plasma-desktop"
+apps="sddm
+    cups
+    plasma-pa
+    plasma-nm
+    plasma-discover
+    plasma-discover-backend-flatpak
+    flatpak
+    fwupd
+    network-manager
+    plymouth
+    plymouth-themes
+    git
+    rsync
+    borgbackup"
+
+if [ ! -x /usr/sbin/sddm ]
+then
+    read -p "Repo Update" -t 2
+    echo -e "\n"
+    apt update
+
+    read -p "Installing $gs" -t 2
+    echo -e "\n"
+    apt install -y $ksd
+
+    read -p "Installing $apps" -t 2
+    echo -e "\n"
+    apt install -y $apps
+
+    read -p "Cleaning up" -t 2
+    echo -e "\n"
+    systemctl enable sddm cups
+    flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    sed -i 's/false/true/g' /etc/NetworkManager/NetworkManager.conf
+
+else
+    read -p "Everything's installed  " -t 2
+fi
