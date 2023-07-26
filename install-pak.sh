4b90a4b0c97d4c57b3a6c578d730aa151e612a2f
khoaled <56563374+khoaled@users.noreply.github.com>
2023-07-26 07:21:18 -0500

    Update install-pak.sh

--- install-pak.sh
+++ install-pak.sh
@@ -2,7 +2,7 @@
 #
 # Micah's Flatpak install script
 #
-paks="org.mozilla.firefox
+gpaks="org.mozilla.firefox
       org.chromium.Chromium
       com.brave.Browser
       com.spotify.Client
@@ -17,8 +17,17 @@ paks="org.mozilla.firefox
       org.gnome.Calculator
       org.gnome.clocks"
 
-
-if [ ! -x /usr/bin/flatpak ]
+kpaks="org.mozilla.firefox
+      org.chromium.Chromium
+      com.brave.Browser
+      com.spotify.Client
+      com.bitwarden.desktop
+      com.visualstudio.code
+      com.mojang.Minecraft
+      org.libreoffice.LibreOffice
+      com.raggesilver.BlackBox"
+ 
+if [ ! -x /usr/bin/flatpak ] && [ -x /usr/bin/gdm ]
 then
     read -p "Installing flatpak and the flatpak repo" -t 2
     echo -e "\n"
@@ -27,8 +36,18 @@ then
 
     read -p "Installing flatpaks" -t 2
     echo -e "\n"
-    flatpak install -y $paks
+    flatpak install -y $gpaks
+elif [ ! -x /usr/bin/flatpak ] && [ -x /usr/bin/sddm ] 
+then
+    read -p "Installing flatpak and the flatpak repo" -t 2
+    echo -e "\n"
+    sudo apt install -y flatpak 
+    sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
+    read -p "Installing flatpaks" -t 2
+    echo -e "\n"
+    flatpak install -y $kpaks
+    
 else
     read -p "Installing flatpaks" -t 2
     echo -e "\n"
