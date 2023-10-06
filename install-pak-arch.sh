9b0ed9cdb87022c7f9722aa522f02d3fdecf793e
khoaled <56563374+khoaled@users.noreply.github.com>
2023-10-06 13:38:54 -0500

    Create install-pak-arch.sh

---
+++ install-pak-arch.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# Micah's Flatpak install script
+#
+nope="--noconfirm"
+
+paks="org.mozilla.firefox
+      org.chromium.Chromium
+      com.brave.Browser
+      com.spotify.Client
+      com.bitwarden.desktop
+      com.visualstudio.code
+      com.mojang.Minecraft
+      org.libreoffice.LibreOffice
+      com.raggesilver.BlackBox
+      org.gnome.eog
+      org.gnome.Weather
+      org.gnome.Calculator
+      org.gnome.clocks"
+
+
+if [ ! -x /usr/bin/flatpak ]
+then
+    read -p "Installing flatpak and the flatpak repo" -t 2
+    echo -e "\n"
+    sudo pacman -S $nope flatpak
+    sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+    read -p "Installing flatpaks" -t 2
+    echo -e "\n"
+    flatpak install -y $paks
+
+else
+    read -p "Installing flatpaks" -t 2
+    echo -e "\n"
+    flatpak install -y $paks
+fi
