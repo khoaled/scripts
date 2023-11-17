c2c88354e9e561131eada640854544e637fbf2bf
khoaled <56563374+khoaled@users.noreply.github.com>
2023-11-17 17:50:59 -0600

    Update install-pak-arch.sh

--- install-pak-arch.sh
+++ install-pak-arch.sh
@@ -6,17 +6,15 @@ nope="--noconfirm"
 
 paks="org.mozilla.firefox
       org.chromium.Chromium
-      com.brave.Browser
+      com.microsoft.Edge
       com.spotify.Client
       com.bitwarden.desktop
       com.visualstudio.code
       com.mojang.Minecraft
       org.libreoffice.LibreOffice
       com.raggesilver.BlackBox
-      org.gnome.eog
-      org.gnome.Weather
-      org.gnome.Calculator
-      org.gnome.clocks"
+      org.gnome.eog"
+
 
 
 if [ ! -x /usr/bin/flatpak ]
